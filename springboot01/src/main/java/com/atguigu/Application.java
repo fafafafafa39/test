e97437a405531f6a5ec5471df3651effa3313675
xuze <570685245@qq.com>
2019-05-09 21:29:08 +0800

    first commit

---
+++ springboot01/src/main/java/com/atguigu/Application.java
@@ -0,0 +1,14 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        // Spring应用启动起来
+        SpringApplication.run(Application.class, args);
+    }
+}
